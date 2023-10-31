0e7ec5f08396cc09ef25792b7300f4487c3ca213
rich-tribeweb <rich@tribeweb.co.uk>
2023-10-31 09:44:34 +0000

    chore: turn into full nuxt project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,24 @@
 export default defineNuxtConfig({
-  extends: 'content-wind',
   modules: [
+    '@nuxt/content',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    'nuxt-icon',
     '@nuxthq/studio'
-  ]
+  ],
+  // https://color-mode.nuxtjs.org
+  colorMode: {
+    classSuffix: ''
+  },
+  // https://content.nuxtjs.org
+  content: {
+    documentDriven: true,
+    highlight: {
+      // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
+      theme: {
+        dark: 'github-dark',
+        default: 'github-light'
+      }
+    }
+  }
 })
